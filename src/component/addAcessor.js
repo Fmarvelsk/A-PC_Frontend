5d4a872b89073e7dfb71911f0fc03dc3aa5256a2
Marvellous <fmarvelsk@gmail.com>
2020-07-23 15:58:57 +0100

    works

--- src/component/addAcessor.js
+++ src/component/addAcessor.js
@@ -42,36 +42,25 @@ class addAcessor extends Component {
         //with
         // ...prev.Accessor1
         //then do for each of the Accessor objects
-      [`Accessor${key+1}`]: { ...[`prev.Accessor${key+1}`] ,[obj]: value },
+      [`Accessor${key+1}`]:  { ...[`prev.Accessor${key+1}`], [obj]: value },
     }));
   }
-  //   AcessorName = (e) => {
-  //     this.setState(state => { state, [e.target.name]: e.target.value });
-  //     console.log('e')
-  //   };
-  //   AcessorUniversity = (e) => {
-  //     this.setState({ [e.target.name]: e.target.value });
-  //   };
-  //   AcessorEmail = (e) => {
-  //     this.setState({ [e.target.name]: e.target.value });
-  //     console.log('w')
-  //     var reqx = /(@|.com)/g;
-  //     if (reqx.test(e.target.value)) {
-  //       document.getElementById("name").innerHTML = "";
-  //     } else {
-  //       document.getElementById("name").innerHTML = "Enter a valid email";
-  //     }
-  //   };
-
-  //   AcessorNumber = (e) => {
-  //     this.setState({ [e.target.name]: e.target.value });
-  //   };
-  //   AcessorNumber1 = (e) => {
-  //     this.setState({ [e.target.name]: e.target.value });
-  //   };
-  //   AcessorName1 = (e) => {
-  //     this.setState({ [e.target.name]: e.target.value });
-  //   };
+  addAddAcessor= (e) => {
+    e.preventDefault()
+    const newCandidate = {
+        Fullname : this.state.Accessor1.Fullname,
+        University : this.state.Accessor1.University,
+        Email : this.state.Accessor1.Email,
+        Number: this.state.Accessor1.Number,
+    }
+    console.log(newCandidate)
+    this.setState({
+        Fullname: "",
+        University: "",
+        Email: "",
+        Number: "",
+    })
+}
 
   render() {
     return (
@@ -79,7 +68,9 @@ class addAcessor extends Component {
         <div className="candidate mt-5">
           <h3>Candidate name</h3>
         </div>
+        <form onSubmit={this.AddAcessor}>
         <div className="row">
+        
           {Object.values(this.state).map((candidate, key) => (
             <div className="col-sm-4 col-md-4 col-xl-4 mb-5" key={key}>
               <div className="width">
@@ -115,112 +106,19 @@ class addAcessor extends Component {
               </div>
             </div>
           ))}
-          {/* <div className="col-sm-4 col-md-4 col-xl-4 mb-5">
-            <div className="width">
-              <input
-                type="text"
-                value={this.state.Accessor.Fullname}
-                name="Fullname"
-                onChange={this.AcessorName}
-                placeholder="Fullname"
-              />
-              <input
-                type="text"
-                value={this.state.Accessor.University}
-                name="University"
-                onChange={this.AcessorUniversity}
-                placeholder="University"
-              />
-              <input
-                type="text"
-                value={this.state.Accessor.Email}
-                name="Email"
-                onChange={this.AcessorEmail}
-                placeholder="Email"
-              />
-              <div id="name"></div>
-              <input
-                type="tel"
-                value={this.state.Accessor.Number}
-                name="Number"
-                onChange={this.AcessorNumber}
-                placeholder="Number"
-              />
-            </div>
-          </div>
-          <div className="col-sm-4 col-md-4 col-xl-4 col-lg-4 mb-5">
-            <div className="width">
-              <input
-                type="text"
-                value={this.state.Accessor.Fullname}
-                name="Fullname"
-                onChange={this.AcessorName1}
-                placeholder="Fullname"
-              />
-              <input
-                type="text"
-                value={this.state.Accessor.University}
-                name="University"
-                onChange={this.AcessorUniversity1}
-                placeholder="University"
-              />
-              <input
-                type="text"
-                value={this.state.Accessor.Email}
-                name="Email"
-                onChange={this.AcessorEmail1}
-                placeholder="Email"
-              />
-              <input
-                type="tel"
-                value={this.state.Accessor.Number}
-                name="Number"
-                onChange={this.AcessorNumber1}
-                placeholder="Number"
-              />
-            </div>
-          </div>
-          <div className="col-sm-4 col-md-4 col-xl-4 col-lg-4 mb-5">
-            <div className="width">
-              <input
-                type="text"
-                value={this.state.Accessor.Fullname}
-                name="Fullname"
-                onChange={this.AcessorName2}
-                placeholder="Fullname"
-              />
-              <input
-                type="text"
-                value={this.state.Accessor.University}
-                name="University"
-                onChange={this.AcessorUniversity2}
-                placeholder="University"
-              />
-              <input
-                type="text"
-                value={this.state.Accessor.Email}
-                name="Email"
-                onChange={this.AcessorEmail2}
-                placeholder="Email"
-              />
-              <input
-                type="tel"
-                value={this.state.Accessor.Number}
-                name="Number"
-                onChange={this.AcessorNumber2}
-                placeholder="Number"
-              />
-            </div>
-          </div> */}
+          {
+
+          }
+         
         </div>
 
         <button
           className="btnSave mt-5"
-          type="button"
-          onSubmit={this.AddAcessor}
+          type="submit"
         >
           Save&Continue
         </button>
+        </form>
       </div>
     );
   }
